fd938690b198bfb5ff55b9dd6b9c6d7181b2da89
AshMW <contact@ashmw.com>
2022-05-16 19:27:09 -0400

    Bruh

---
+++ src/pages/general/home/index.jsx
@@ -0,0 +1,5 @@
+<>
+  <Heading fontSize="5xl" bg={themeColors['brand']['500']}>Welcome</Heading>
+  <Text fontSize="xl" opacity={0.8}>Welcome to the HFD Resource center.</Text>
+  <Divider mt={6} />
+</>
